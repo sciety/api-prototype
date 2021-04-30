fb11d998535700cc01c0658e7fbc6576b033b49f
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-04-30 14:41:09 +0100

    Show list of recommendations

--- client/web/index.php
+++ client/web/index.php
@@ -7,6 +7,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $articleId = 'sciety:pigs-article';
 
+RdfNamespace::set('cito', 'http://purl.org/spar/cito/');
 RdfNamespace::set('fabio', 'http://purl.org/spar/fabio/');
 RdfNamespace::set('frbr', 'http://purl.org/vocab/frbr/core#');
 RdfNamespace::set('sciety', 'http://localhost:8080/');
@@ -16,6 +17,19 @@ $sparql = new Client("http://jena:3030/sciety");
 $article = $sparql->query(<<<SPARQL
 DESCRIBE {$articleId}
 SPARQL)->resource($articleId);
+
+$recommendations = iterator_to_array($sparql->query(<<<SPARQL
+SELECT *
+
+WHERE {
+  ?item cito:citesAsRecommendedReading {$articleId} .
+  ?expression frbr:realizationOf ?item .
+  ?expression dcterms:publisher ?publisher .
+  ?publisher rdfs:label ?publisherLabel .
+}
+
+ORDER BY ASC(?publisherLabel)
+SPARQL));
 
 $versions = $sparql->query(<<<SPARQL
 SELECT *
@@ -41,6 +55,13 @@ SPARQL);
 
 echo "<!doctype html><title>{$article->label()}</title><body><h1>{$article->label()}</h1>";
 
+if(count($recommendations)) {
+    echo '<p><strong>Recommended by: ';
+    echo implode(', ', array_map(fn($recommendation) => $recommendation->publisherLabel->getValue(), $recommendations));
+    echo '</strong>';
+}
+
+echo '<hr>';
 echo '<table><thead><tr><th>Version<th>Date<th>Published by<th>DOI</tr></thead><tbody>';
 
 foreach ($versions as $version) {
