995bbb4cc7e890f70d3273d6d126416a2e31fce1
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-04-30 14:26:58 +0100

    Add list of versions

--- client/web/index.php
+++ client/web/index.php
@@ -7,6 +7,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $articleId = 'sciety:pigs-article';
 
+RdfNamespace::set('fabio', 'http://purl.org/spar/fabio/');
 RdfNamespace::set('frbr', 'http://purl.org/vocab/frbr/core#');
 RdfNamespace::set('sciety', 'http://localhost:8080/');
 
@@ -16,4 +17,43 @@ $article = $sparql->query(<<<SPARQL
 DESCRIBE {$articleId}
 SPARQL)->resource($articleId);
 
+$versions = $sparql->query(<<<SPARQL
+SELECT *
+
+WHERE {
+  ?item frbr:realizationOf {$articleId} .
+  ?item dcterms:date ?date .
+  ?item dcterms:publisher ?publisher .
+  OPTIONAL {
+    ?item dcterms:title ?title .
+  }
+  OPTIONAL {
+    ?item dcterms:identifier ?doi .
+    FILTER(strStarts(?doi, 'doi:')) .
+  }
+  ?publisher rdfs:label ?publisherLabel .
+  ?item fabio:hasManifestation ?manifestation .
+  ?manifestation fabio:hasURL ?manifestationUrl .
+}
+
+ORDER BY ASC(?date)
+SPARQL);
+
 echo "<!doctype html><title>{$article->label()}</title><body><h1>{$article->label()}</h1>";
+
+echo '<table><thead><tr><th>Version<th>Date<th>Published by<th>DOI</tr></thead><tbody>';
+
+foreach ($versions as $version) {
+    $title = isset($version->title) ? $version->title->getValue() : '(no title)';
+    $doi = isset($version->doi) ? substr($version->doi->getValue(), 4) : null;
+    $url = $doi ? "https://doi.org/${doi}" : $version->manifestationUrl->getValue();
+    echo <<<HTML
+<tr>
+<th><a href="{$url}">{$title}</a>
+<td>{$version->date->format('j F Y')}
+<td>{$version->publisherLabel->getValue()}
+<td>{$doi}
+HTML;
+}
+
+echo '</table>';
