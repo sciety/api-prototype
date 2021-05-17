7eabd087b2d26ead9665e9c670d7bd68b056570d
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-05-17 11:04:38 +0100

    Remove unused code

--- update/src/string.ts
+++ update/src/string.ts
@@ -1,13 +1,5 @@
-import { flow, pipe } from 'fp-ts/function'
-import { concatAll, intercalate } from 'fp-ts/Semigroup'
 import * as S from 'fp-ts/string'
 
 export * from 'fp-ts/string'
-
-export const joinWith = (middle: string) => pipe(S.Semigroup, intercalate(middle))
-
-export const joinAllWith = flow(joinWith, concatAll)
-
-export const join = (middle: string) => pipe(S.empty, joinAllWith(middle))
 
 export const prependWith = (prepended: string) => (string: string) => S.Semigroup.concat(prepended, string)
