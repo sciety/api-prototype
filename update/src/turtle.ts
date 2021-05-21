234ead509e4bce303e409a07be57cb3193ce7ded
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-05-21 14:40:49 +0100

    Use a pipe

--- update/src/turtle.ts
+++ update/src/turtle.ts
@@ -1,45 +1,34 @@
 import { serializers } from '@rdfjs-elements/formats-pretty'
+import * as E from 'fp-ts/Either'
 import { flow, pipe } from 'fp-ts/function'
 import * as RR from 'fp-ts/ReadonlyRecord'
-import * as T from 'fp-ts/Task'
+import * as TE from 'fp-ts/TaskEither'
 import pEvent from 'p-event'
 import { Writable } from 'stream'
 import * as D from './dataset'
 import * as fs from './fs'
 import * as RDF from './rdf'
 import * as S from './string'
-import * as E from 'fp-ts/Either'
-import * as TE from 'fp-ts/TaskEither'
 
-const writeTo = (stream: Writable) => (prefixes: {
+const graphPrefixes = RR.map((namespace: RDF.NamedNode) => namespace.value)
+
+const writeTo = (stream: Writable) => <A extends RDF.Quad>(prefixes: {
   [key: string]: RDF.NamedNode
-}) => <A extends RDF.Quad>(dataset: D.DatasetCore<A, RDF.BaseQuad>): TE.TaskEither<Error, void> => {
-  const graphPrefixes = pipe(
-    prefixes,
-    RR.map(namespace => namespace.value),
-  )
-
-  const quadStream = pipe(
-    dataset,
-    D.toStreamMap<A>(RDF.ord, RDF.toTriple),
-  )
-
-  const serialized = serializers.import('text/turtle', quadStream, { prefixes: graphPrefixes })
-
-  if (!serialized) {
-    return TE.left(new Error('Serializer not initialised'))
-  }
-
-  serialized.on('data', flow(
-    String,
-    S.replaceAll('\t', '  '),
-    data => stream.write(data),
-  ))
-
-  serialized.on('end', () => stream.end())
-
-  return TE.tryCatch(() => pEvent(stream, 'finish'), E.toError)
-}
+}): (dataset: D.DatasetCore<A, RDF.BaseQuad>) => TE.TaskEither<Error, void> => flow(
+  D.toStreamMap<A>(RDF.ord, RDF.toTriple),
+  quadStream => serializers.import('text/turtle', quadStream, { prefixes: pipe(prefixes, graphPrefixes) }),
+  E.fromNullable(new Error('Serializer not initialised')),
+  TE.fromEither,
+  TE.chainFirstIOK(serialized => () => serialized
+    .on('data', flow(
+      String,
+      S.replaceAll('\t', '  '),
+      data => stream.write(data),
+    ))
+    .on('end', () => stream.end())
+  ),
+  TE.chain(() => TE.tryCatch(() => pEvent(stream, 'finish'), E.toError)),
+)
 
 export const writeToFile = flow(
   fs.createWriteStream,
