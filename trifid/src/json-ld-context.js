5f113eb77d0382e4b1aa9bc148038bf08ca4eeb4
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-05-24 14:28:39 +0100

    Serve a separate JSON-LD context

--- trifid/src/json-ld-context.js
+++ trifid/src/json-ld-context.js
@@ -4,7 +4,24 @@ const hijackResponse = require('hijackresponse')
 const jsonld = require('jsonld')
 const {Readable} = require('stream')
 const toString = require('stream-to-string')
-const context = require('./context.json')
+
+const contexts = {
+  'http://localhost:8080/context.jsonld': require('./context.json')
+};
+
+const nodeDocumentLoader = jsonld.documentLoaders.node();
+
+const contextsLoader = async (url) => {
+  if (url in contexts) {
+    return {
+      contextUrl: null,
+      document: contexts[url],
+      documentUrl: url,
+    };
+  }
+
+  return nodeDocumentLoader(url);
+};
 
 const middleware = (req, res, next) => {
   hijackResponse(res, next).then(({destroyAndRestore, readable, writable}) => {
@@ -17,7 +34,7 @@ const middleware = (req, res, next) => {
     return toString(readable)
       .then(JSON.parse)
       .then(doc => jsonld.frame(doc, { '@id': req.iri }, { omitGraph: true }))
-      .then(doc => jsonld.compact(doc, context))
+      .then(doc => jsonld.compact(doc, 'http://localhost:8080/context.jsonld', { documentLoader: contextsLoader }))
       .then(JSON.stringify)
       .then(Readable.from)
       .then(stream => stream.pipe(writable))
