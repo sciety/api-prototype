88fb53b31a399451f875d7fa3189bd8f0f3ff276
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-07-13 08:58:00 +0100

    Update types

--- schema/@types/rdf-utils-dataset/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'rdf-utils-dataset' {
-
-  import * as RDF from 'rdf-js'
-  import DatasetExt from 'rdf-ext/lib/Dataset';
-
-  export const resource: <OutQuad extends RDF.BaseQuad = RDF.Quad, InQuad extends RDF.BaseQuad = RDF.Quad, D extends DatasetExt<OutQuad, InQuad>>(input: D, subject: OutQuad['subject']) => D
-}
